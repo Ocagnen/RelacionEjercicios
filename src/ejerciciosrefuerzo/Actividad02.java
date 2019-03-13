8b51540a9fba3eb0ee3ec09eb40b78a3bc344d75
javier <ocagnen2@gmail.com>
2019-03-13 18:51:07 +0100

    Recorrer un array para mostrar su contenido

---
+++ src/ejerciciosrefuerzo/Actividad02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosrefuerzo;
+
+/**
+ *
+ * @author Javier
+ */
+public class Actividad02 {
+    
+    public static void main(String[] args) {       
+          
+    String[] meses =  {"Enero","Febrero","Marzo","Abril",
+"Mayo","Junio","Julio","Agosto", "Septiembre","Octubre","Noviembre",
+"Diciembre"};
+    
+        for (int i = 0; i < meses.length; i++) {
+            System.out.println(meses[i]);
+        }
+    
+    }
+  
+}
