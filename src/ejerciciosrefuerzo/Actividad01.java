950793652ae611b74b222ef705b1542fae4d4b5b
javier <ocagnen2@gmail.com>
2019-03-13 18:47:50 +0100

    Actividad 1

---
+++ src/ejerciciosrefuerzo/Actividad01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosrefuerzo;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Javier
+ */
+public class Actividad01 {
+    
+    public static void main(String[] args) {
+        
+        int[] dias = {1,2,3,4,5,6,7,8,9,10};
+        
+        System.out.println(Arrays.binarySearch(dias, 7));
+        
+        
+    }
+    
+}
